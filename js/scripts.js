f75eec86e6a2b550b9c33a72be9c43a67711ec16
Anastasia Fazius <AnastasiaFazius@gmx.de>
2023-06-28 02:24:50 +0200

    Creates a variable pokemonList, array of 5 Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-
-let favoriteFood = 'Pho';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Nidorino', attack: 72, defense: 57, type: 'poison'},
+    {name: 'Ponyta', attack: 85, defense: 55, type: 'fire'},
+    {name: 'Onix', attack: 45, defense: 160, type: 'rock' + 'ground'},
+    {name: 'Starmie', attack: 75, defense: 85, type: 'water' + 'psychic'},
+    {name: 'Umbreon', attack: 65, defense: 110, type: 'dark'},
+];
