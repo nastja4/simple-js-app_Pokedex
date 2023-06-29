eb5ddcf0c129d3a451d93aaa48cf93e635002829
Anastasia Fazius <AnastasiaFazius@gmx.de>
2023-06-29 09:02:38 +0200

    Correct the key "type" to "types"

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    {name: 'Nidorino', attack: 72, defense: 57, type: 'poison'},
-    {name: 'Ponyta', attack: 85, defense: 55, type: 'fire'},
-    {name: 'Onix', attack: 45, defense: 160, type: ['rock', 'ground']},
-    {name: 'Starmie', attack: 75, defense: 85, type: ['water', 'psychic']},
-    {name: 'Umbreon', attack: 65, defense: 110, type: 'dark'},
+    {name: 'Nidorino', attack: 72, defense: 57, types: 'poison'},
+    {name: 'Ponyta', attack: 85, defense: 55, types: 'fire'},
+    {name: 'Onix', attack: 45, defense: 160, types: ['rock', 'ground']},
+    {name: 'Starmie', attack: 75, defense: 85, types: ['water', 'psychic']},
+    {name: 'Umbreon', attack: 65, defense: 110, types: 'dark'},
 ];
