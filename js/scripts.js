98fca3de074f0fe707a1ece89f1c991728ec1877
Anastasia Fazius <AnastasiaFazius@gmx.de>
2023-07-05 03:05:12 +0200

    (not finished) forEach() Loop; wrap in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,61 @@
-let pokemonList = [
-    {name: 'Nidorino', attack: 72, defense: 57, types: ['poison']},
-    {name: 'Ponyta', attack: 85, defense: 55, types: ['fire']},
-    {name: 'Onix', attack: 45, defense: 160, types: ['rock', 'ground']},
-    {name: 'Starmie', attack: 75, defense: 85, types: ['water', 'psychic']},
-    {name: 'Umbreon', attack: 65, defense: 110, types: ['dark']},
-];
 
-function printArrayDetails() {
-    for (let i = 0; i < pokemonList.length; i++) {    
-        if (pokemonList[i].attack > 80) {        
-            document.write(pokemonList[i].name + ' (attack: ' + pokemonList[i].attack + ' / defense: ' + pokemonList[i].defense + ') is the most powerful pokemon of ' + pokemonList.length + '<br />'); 
-            } else {
-                document.write(pokemonList[i].name + ' (attack: ' + pokemonList[i].attack + ' / defense: ' + pokemonList[i].defense + ')<br/>');
-            }
+// let pokemonList = [
+//     {name: 'Nidorino', attack: 72, defense: 57, types: ['poison']},
+//     {name: 'Ponyta', attack: 85, defense: 55, types: ['fire']},
+//     {name: 'Onix', attack: 45, defense: 160, types: ['rock', 'ground']},
+//     {name: 'Starmie', attack: 75, defense: 85, types: ['water', 'psychic']},
+//     {name: 'Umbreon', attack: 65, defense: 110, types: ['dark']}
+// ];
+
+// function printArrayDetails() {
+//     for (let i = 0; i < pokemonList.length; i++) {    
+//         if (pokemonList[i].attack > 80) {        
+//             document.write(pokemonList[i].name + ' (attack: ' + pokemonList[i].attack + ' / defense: ' + pokemonList[i].defense + ') is the most powerful pokemon of ' + pokemonList.length + '<br>'); 
+//             } else {
+//                 document.write(pokemonList[i].name + ' (attack: ' + pokemonList[i].attack + ' / defense: ' + pokemonList[i].defense + ')<br>');
+//             }
+//     }
+// }
+
+// printArrayDetails();
+
+
+
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Nidorino', attack: 72, defense: 57, types: ['poison']},
+        {name: 'Ponyta', attack: 85, defense: 55, types: ['fire']},
+        {name: 'Onix', attack: 45, defense: 160, types: ['rock', 'ground']},
+        {name: 'Starmie', attack: 75, defense: 85, types: ['water', 'psychic']},
+        {name: 'Umbreon', attack: 65, defense: 110, types: ['dark']}
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-}
 
-printArrayDetails();
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    
+    return {
+        getAll: getAll,
+        add: add
+    };    
+})();
+
+
+function pokemonLoopFunction(pokemon) {           
+    if (pokemon.attack > 80) {        
+        document.write(pokemon.name + ' (attack: ' + pokemon.attack + ', defense: ' + pokemon.defense + ', types: ' + pokemon.types + ') is the most powerful pokemon of ' + pokemonList.length + '<br>'); 
+        } else {
+            document.write(pokemon.name + ' (attack: ' + pokemon.attack + ', defense: ' + pokemon.defense + ', types: ' + pokemon.types + ')<br>');
+        }
+}
+// pokemonList.forEach(pokemonLoopFunction);
+pokemonRepository.getAll().forEach(pokemonLoopFunction);
+
+
+// document.write(pokemonRepository.getAll());
+// pokemonRepository.add('RRR');
+// document.write(pokemonRepository.getAll());
